f06f2c3995a640a706ef1bf13b8014bd9875b2b4
fenghao12345 <liuf0031@e.ntu.edu.sg>
2022-04-16 23:50:35 +0800

    delete

--- OneTracker/base/forms.py
+++
@@ -1,9 +0,0 @@
-from django.forms import ModelForm
-from .models import IngredientStock
-
-
-class IngredientForm(ModelForm):
-    class Meta:
-        model = IngredientStock
-        fields = '__all__'
-        exclude = ["user", "name", "ingredient_id"]
